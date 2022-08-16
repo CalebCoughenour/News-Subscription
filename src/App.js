896cfe63c4dad1b5b1639a87544194c24565a4a5
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-16 10:48:21 -0700

    add imports and route for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/HomePage/Home';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello Worlds!
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
