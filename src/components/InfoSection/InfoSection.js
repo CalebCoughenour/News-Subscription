257c1b2c088356c7619dbe56bf05fb7df9ef4383
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-16 14:03:49 -0700

    add image with wrapper

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Container } from '../../globalStyles';
-import { InfoSec, InfoRow, InfoColumn, TextWrapper, Heading, Subtitle } from './InfoSection.elements';
+import { InfoSec, InfoRow, InfoColumn, TextWrapper, Heading, Subtitle, Img, ImgWrapper } from './InfoSection.elements';
 
 
-const InfoSection = ({ lightBg, imgStart, lightTextDesc, description, headline, lightText }) => {
+const InfoSection = ({ lightBg, imgStart, lightTextDesc, description, headline, lightText, img, alt, start }) => {
   return (
     <>
       <InfoSec lightBg={lightBg}>
         <Container>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <InfoColumn>
               <TextWrapper>
                 <Heading lightText={lightText}>{headline}</Heading>
                 <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
               </TextWrapper>
+            </InfoColumn>
+            <InfoColumn>
+              <ImgWrapper start={start}>
+                <Img src={img} alt={alt} />
+              </ImgWrapper>
             </InfoColumn>
           </InfoRow>
         </Container>
