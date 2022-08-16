8a7a6e1fd068508007d0d2faeff293c68b17244f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-16 13:33:20 -0700

    add data for homepage info section

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,10 @@
+export const homeObjOne = {
+  lightBg: false,
+  primary: true,
+  lightTopLine: true,
+  lightTextDesc: true,
+  description: 'Your #1 source for the world\'s current events, sports, health and so much more! See our subscription tiers below for more on how to sign up.',
+  headline: 'Daily News Feed Curated For You',
+  lightText: true,
+  start: ''
+};
