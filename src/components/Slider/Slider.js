8b1f18aa8f405e1820d900418d15b175df1de0ce
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-16 18:16:46 -0700

    add code for bootstrap slider with 3 images

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,45 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function Slider() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={1700}>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1526470608268-f674ce90ebd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          alt="Fish eye photo of a city"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1700}>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1560177112-fbfd5fde9566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Three newspapers hanging on a brick wall"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1700}>
+        <img
+          className="d-block w-100 "
+          src="https://images.unsplash.com/photo-1573812195421-50a396d17893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80"
+          alt="Stack of newspapers"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slider;
