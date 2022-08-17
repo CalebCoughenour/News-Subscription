b7f41ec109fe287003131f16c9d7c7b6882ca06d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-16 18:09:06 -0700

    add navbar and bootstrap slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Footer } from "./components";
+import { Footer, Navbar, Slider } from "./components";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Home from './pages/HomePage/Home';
@@ -11,6 +12,8 @@ function App() {
     <div>
       <Router>
         <GlobalStyle />
+        <Navbar />
+        <Slider />
         <Routes>
           <Route path='/' exact element={<Home />} />
         </Routes>
