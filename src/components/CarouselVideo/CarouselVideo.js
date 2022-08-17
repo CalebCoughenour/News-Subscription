94a63fc39e59e74f2b395cc694ab2cb21a359962
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-17 12:45:07 -0700

    add styling to video to make it fit in container

--- src/components/CarouselVideo/CarouselVideo.js
+++ src/components/CarouselVideo/CarouselVideo.js
@@ -1,12 +1,16 @@
+import React from 'react';
+
 const src =
   "https://assets.mixkit.co/videos/preview/mixkit-world-map-in-digital-environment-12742-large.mp4";
 
 const CarouselVideo = () => {
   return (
-    <video loop muted autoPlay controls = '' width="100%">
-      <source src={src} type="video/mp4" />
-      Sorry, your browser doesn't support embedded videos.
-    </video>
+    
+      <video loop muted autoPlay width="100%" style={{ height: "600px", objectFit: "cover", marginBottom: "0px"}}>
+        <source src={src} type="video/mp4" />
+        Sorry, your browser doesn't support embedded videos.
+      </video>
+    
   );
 };
 
